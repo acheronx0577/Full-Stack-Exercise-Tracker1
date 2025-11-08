3ff0a9128f01882a6a88cc8c89b71d1230217155
AcheronX. <richardhuynh53@gmail.com>
2025-11-08 12:07:15 -0800

    Fixed bugs

---
+++ models/Exercise.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Exercise', exerciseSchema);
